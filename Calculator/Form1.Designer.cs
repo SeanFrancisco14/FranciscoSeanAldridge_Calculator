47f0bdfb8605ac06aee625531f2af0b483baee83
Sean Aldridge Francisco <seanaldridgemfrancisco@iskolarngbayan.pup.edu.ph>
2021-05-01 08:35:30 +0800

    OOP

---
+++ Calculator/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿
+namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.num7 = new System.Windows.Forms.Button();
+            this.num8 = new System.Windows.Forms.Button();
+            this.num9 = new System.Windows.Forms.Button();
+            this.num4 = new System.Windows.Forms.Button();
+            this.num5 = new System.Windows.Forms.Button();
+            this.num6 = new System.Windows.Forms.Button();
+            this.num1 = new System.Windows.Forms.Button();
+            this.num2 = new System.Windows.Forms.Button();
+            this.num3 = new System.Windows.Forms.Button();
+            this.num0 = new System.Windows.Forms.Button();
+            this.period = new System.Windows.Forms.Button();
+            this.positiveOrnegative = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.multiplicationSign = new System.Windows.Forms.Button();
+            this.divisionSign = new System.Windows.Forms.Button();
+            this.minusSign = new System.Windows.Forms.Button();
+            this.equalsSign = new System.Windows.Forms.Button();
+            this.plusSign = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // num7
+            // 
+            this.num7.Location = new System.Drawing.Point(12, 63);
+            this.num7.Name = "num7";
+            this.num7.Size = new System.Drawing.Size(59, 50);
+            this.num7.TabIndex = 0;
+            this.num7.Text = "7";
+            this.num7.UseVisualStyleBackColor = true;
+            // 
+            // num8
+            // 
+            this.num8.Location = new System.Drawing.Point(90, 63);
+            this.num8.Name = "num8";
+            this.num8.Size = new System.Drawing.Size(59, 50);
+            this.num8.TabIndex = 1;
+            this.num8.Text = "8";
+            this.num8.UseVisualStyleBackColor = true;
+            // 
+            // num9
+            // 
+            this.num9.Location = new System.Drawing.Point(171, 63);
+            this.num9.Name = "num9";
+            this.num9.Size = new System.Drawing.Size(59, 50);
+            this.num9.TabIndex = 2;
+            this.num9.Text = "9";
+            this.num9.UseVisualStyleBackColor = true;
+            // 
+            // num4
+            // 
+            this.num4.Location = new System.Drawing.Point(12, 132);
+            this.num4.Name = "num4";
+            this.num4.Size = new System.Drawing.Size(59, 50);
+            this.num4.TabIndex = 3;
+            this.num4.Text = "4";
+            this.num4.UseVisualStyleBackColor = true;
+            // 
+            // num5
+            // 
+            this.num5.Location = new System.Drawing.Point(90, 132);
+            this.num5.Name = "num5";
+            this.num5.Size = new System.Drawing.Size(59, 50);
+            this.num5.TabIndex = 4;
+            this.num5.Text = "5";
+            this.num5.UseVisualStyleBackColor = true;
+            // 
+            // num6
+            // 
+            this.num6.Location = new System.Drawing.Point(171, 131);
+            this.num6.Name = "num6";
+            this.num6.Size = new System.Drawing.Size(59, 50);
+            this.num6.TabIndex = 5;
+            this.num6.Text = "6";
+            this.num6.UseVisualStyleBackColor = true;
+            // 
+            // num1
+            // 
+            this.num1.Location = new System.Drawing.Point(12, 201);
+            this.num1.Name = "num1";
+            this.num1.Size = new System.Drawing.Size(59, 50);
+            this.num1.TabIndex = 6;
+            this.num1.Text = "1";
+            this.num1.UseVisualStyleBackColor = true;
+            // 
+            // num2
+            // 
+            this.num2.Location = new System.Drawing.Point(90, 201);
+            this.num2.Name = "num2";
+            this.num2.Size = new System.Drawing.Size(59, 50);
+            this.num2.TabIndex = 7;
+            this.num2.Text = "2";
+            this.num2.UseVisualStyleBackColor = true;
+            this.num2.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // num3
+            // 
+            this.num3.Location = new System.Drawing.Point(171, 201);
+            this.num3.Name = "num3";
+            this.num3.Size = new System.Drawing.Size(59, 50);
+            this.num3.TabIndex = 8;
+            this.num3.Text = "3";
+            this.num3.UseVisualStyleBackColor = true;
+            // 
+            // num0
+            // 
+            this.num0.Location = new System.Drawing.Point(12, 273);
+            this.num0.Name = "num0";
+            this.num0.Size = new System.Drawing.Size(137, 50);
+            this.num0.TabIndex = 9;
+            this.num0.Text = "0";
+            this.num0.UseVisualStyleBackColor = true;
+            // 
+            // period
+            // 
+            this.period.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.period.Location = new System.Drawing.Point(171, 273);
+            this.period.Name = "period";
+            this.period.Size = new System.Drawing.Size(59, 50);
+            this.period.TabIndex = 10;
+            this.period.Text = ".";
+            this.period.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.period.UseVisualStyleBackColor = true;
+            // 
+            // positiveOrnegative
+            // 
+            this.positiveOrnegative.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.positiveOrnegative.Location = new System.Drawing.Point(254, 63);
+            this.positiveOrnegative.Name = "positiveOrnegative";
+            this.positiveOrnegative.Size = new System.Drawing.Size(59, 50);
+            this.positiveOrnegative.TabIndex = 11;
+            this.positiveOrnegative.Text = "-/+";
+            this.positiveOrnegative.UseVisualStyleBackColor = true;
+            // 
+            // clearButton
+            // 
+            this.clearButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.clearButton.ForeColor = System.Drawing.Color.Lime;
+            this.clearButton.Location = new System.Drawing.Point(331, 64);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(59, 50);
+            this.clearButton.TabIndex = 12;
+            this.clearButton.Text = "C";
+            this.clearButton.UseVisualStyleBackColor = false;
+            // 
+            // multiplicationSign
+            // 
+            this.multiplicationSign.Location = new System.Drawing.Point(254, 132);
+            this.multiplicationSign.Name = "multiplicationSign";
+            this.multiplicationSign.Size = new System.Drawing.Size(59, 50);
+            this.multiplicationSign.TabIndex = 13;
+            this.multiplicationSign.Text = "X";
+            this.multiplicationSign.UseVisualStyleBackColor = true;
+            // 
+            // divisionSign
+            // 
+            this.divisionSign.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.divisionSign.Location = new System.Drawing.Point(331, 131);
+            this.divisionSign.Name = "divisionSign";
+            this.divisionSign.Size = new System.Drawing.Size(59, 50);
+            this.divisionSign.TabIndex = 14;
+            this.divisionSign.Text = "÷";
+            this.divisionSign.UseVisualStyleBackColor = true;
+            // 
+            // minusSign
+            // 
+            this.minusSign.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minusSign.Location = new System.Drawing.Point(254, 200);
+            this.minusSign.Name = "minusSign";
+            this.minusSign.Size = new System.Drawing.Size(59, 50);
+            this.minusSign.TabIndex = 15;
+            this.minusSign.Text = "-";
+            this.minusSign.UseVisualStyleBackColor = true;
+            // 
+            // equalsSign
+            // 
+            this.equalsSign.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.equalsSign.Location = new System.Drawing.Point(331, 200);
+            this.equalsSign.Name = "equalsSign";
+            this.equalsSign.Size = new System.Drawing.Size(59, 123);
+            this.equalsSign.TabIndex = 16;
+            this.equalsSign.Text = "=";
+            this.equalsSign.UseVisualStyleBackColor = true;
+            // 
+            // plusSign
+            // 
+            this.plusSign.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.plusSign.Location = new System.Drawing.Point(254, 273);
+            this.plusSign.Name = "plusSign";
+            this.plusSign.Size = new System.Drawing.Size(59, 50);
+            this.plusSign.TabIndex = 17;
+            this.plusSign.Text = "+";
+            this.plusSign.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(378, 20);
+            this.textBox1.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(407, 328);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.plusSign);
+            this.Controls.Add(this.equalsSign);
+            this.Controls.Add(this.minusSign);
+            this.Controls.Add(this.divisionSign);
+            this.Controls.Add(this.multiplicationSign);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.positiveOrnegative);
+            this.Controls.Add(this.period);
+            this.Controls.Add(this.num0);
+            this.Controls.Add(this.num3);
+            this.Controls.Add(this.num2);
+            this.Controls.Add(this.num1);
+            this.Controls.Add(this.num6);
+            this.Controls.Add(this.num5);
+            this.Controls.Add(this.num4);
+            this.Controls.Add(this.num9);
+            this.Controls.Add(this.num8);
+            this.Controls.Add(this.num7);
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button num7;
+        private System.Windows.Forms.Button num8;
+        private System.Windows.Forms.Button num9;
+        private System.Windows.Forms.Button num4;
+        private System.Windows.Forms.Button num5;
+        private System.Windows.Forms.Button num6;
+        private System.Windows.Forms.Button num1;
+        private System.Windows.Forms.Button num2;
+        private System.Windows.Forms.Button num3;
+        private System.Windows.Forms.Button num0;
+        private System.Windows.Forms.Button period;
+        private System.Windows.Forms.Button positiveOrnegative;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button multiplicationSign;
+        private System.Windows.Forms.Button divisionSign;
+        private System.Windows.Forms.Button minusSign;
+        private System.Windows.Forms.Button equalsSign;
+        private System.Windows.Forms.Button plusSign;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
