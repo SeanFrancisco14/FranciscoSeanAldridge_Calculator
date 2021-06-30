978ca4984b8d427c037c3a0c705f4b3105efeb7c
Sean Aldridge Francisco <seanaldridgemfrancisco@iskolarngbayan.pup.edu.ph>
2021-06-30 13:40:39 +0800

    OOP

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -5,9 +5,7 @@ namespace Calculator
 {
     public partial class Form1 : Form
     {
-        Double result_Value = 0;
-        string calcu_Operation = "";
-        bool isOperationPerformed = false, isInfinityException = false;
+        calculatorClass calc = new calculatorClass();
 
         public Form1()
         {
@@ -26,64 +24,64 @@ namespace Calculator
 
         private void equalsSign_Click(object sender, EventArgs e)
         {
-            switch(calcu_Operation)
+            switch (calc.Calcu_Operation)
             {
                 case "+":
-                    textBox_Answer.Text = (result_Value + Double.Parse(textBox_Answer.Text)).ToString();
+                    textBox_Answer.Text = (calc.Result_Value + Double.Parse(textBox_Answer.Text)).ToString();
                     break;
 
                 case "-":
-                    textBox_Answer.Text = (result_Value - Double.Parse(textBox_Answer.Text)).ToString();
+                    textBox_Answer.Text = (calc.Result_Value - Double.Parse(textBox_Answer.Text)).ToString();
                     break;
 
                 case "*":
-                    textBox_Answer.Text = (result_Value * Double.Parse(textBox_Answer.Text)).ToString();
+                    textBox_Answer.Text = (calc.Result_Value * Double.Parse(textBox_Answer.Text)).ToString();
                     break;
 
                 case "/":
-                    textBox_Answer.Text = (result_Value / Double.Parse(textBox_Answer.Text)).ToString();
+                    textBox_Answer.Text = (calc.Result_Value / Double.Parse(textBox_Answer.Text)).ToString();
                     break;
                 default:
                     break;
             }
-            result_Value = Double.Parse(textBox_Answer.Text);
+            calc.Result_Value = Double.Parse(textBox_Answer.Text);
             currentOperation_Label.Text = "";
         }
 
         private void button_Click(object sender, EventArgs e)
         {
-            if ((textBox_Answer.Text == "0") || isOperationPerformed)
+            if ((textBox_Answer.Text == "0") || calc.IsOperationPerformed)
                 textBox_Answer.Clear();
 
-           isOperationPerformed = false;
-           Button button = (Button)sender;
+            calc.IsOperationPerformed = false;
+            Button button = (Button)sender;
             if (button.Text == ".")
             {
                 if (!textBox_Answer.Text.Contains("."))
                     textBox_Answer.Text = textBox_Answer.Text + button.Text;
-            }else
-
-           textBox_Answer.Text = textBox_Answer.Text + button.Text;
+            }
+            else
+            textBox_Answer.Text = textBox_Answer.Text + button.Text;
         }
 
         private void operator_Click(object sender, EventArgs e)
         {
             Button button = (Button)sender;
 
-            if (result_Value != 0)
+            if (calc.Result_Value != 0)
             {
                 btnEquals.PerformClick();
-                calcu_Operation = button.Text;
-                result_Value = Double.Parse(textBox_Answer.Text);
-                currentOperation_Label.Text = result_Value + "" + calcu_Operation;
-                isOperationPerformed = true;
+                calc.Calcu_Operation = button.Text;
+                calc.Result_Value = Double.Parse(textBox_Answer.Text);
+                currentOperation_Label.Text = calc.Result_Value + "" + calc.Calcu_Operation;
+                calc.IsOperationPerformed = true;
             }
             else
             {
-                calcu_Operation = button.Text;
-                result_Value = Double.Parse(textBox_Answer.Text);
-                currentOperation_Label.Text = result_Value + "" + calcu_Operation;
-                isOperationPerformed = true;
+                calc.Calcu_Operation = button.Text;
+                calc.Calcu_Operation = Double.Parse(textBox_Answer.Text).ToString();
+                currentOperation_Label.Text = calc.Result_Value + "" + calc.Calcu_Operation;
+                calc.IsOperationPerformed = true;
             }
         }
 
@@ -91,14 +89,14 @@ namespace Calculator
         {
             textBox_Answer.Text = "0";
             currentOperation_Label.Text = "";
-            result_Value = 0;
+            calc.Result_Value = 0;
         }
 
         private void clearButton_Click(object sender, EventArgs e)
         {
             textBox_Answer.Text = "0";
             currentOperation_Label.Text = "";
-            result_Value = 0;
+            calc.Result_Value = 0;
         }
 
         private void textBox_Answer_TextChanged(object sender, EventArgs e)
@@ -108,7 +106,7 @@ namespace Calculator
 
         private void btnSign_Click(object sender, EventArgs e)
         {
-            if (!isInfinityException)
+            if (!calc.IsInfinityException)
             {
                 textBox_Answer.Text = (double.Parse(textBox_Answer.Text) * -1).ToString();
             }
---
+++ Calculator/calculatorClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class calculatorClass
+    {
+        Form1 op = new Form1();
+        private Double result_Value = 0;
+        private string calcu_Operation = "";
+        private bool isOperationPerformed = false;
+        private bool isInfinityException = false;
+
+        public double Result_Value { get => result_Value; set => result_Value = value; }
+        public string Calcu_Operation { get => calcu_Operation; set => calcu_Operation = value; }
+        public bool IsOperationPerformed { get => isOperationPerformed; set => isOperationPerformed = value; }
+        public bool IsInfinityException { get => isInfinityException; set => isInfinityException = value; }
+    }
+}
